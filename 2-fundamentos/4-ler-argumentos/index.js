d12105644ca6fb42c2613ab6072e9a3b30098d89
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-03-21 16:54:30 -0300

    leitura de argumentos módulo interno

---
+++ 2-fundamentos/4-ler-argumentos/index.js
@@ -0,0 +1,19 @@
+// nome, idade, sexo 
+
+console.log(process.argv)
+
+const args = process.argv.slice(2)
+
+console.log(args)
+
+const nome = args[0].split('=')[1]
+
+console.log(nome)
+
+const idade = args[1].split('=')[1]
+
+console.log(idade)
+
+const sexo = args[2].split('=')[1]
+
+console.log(sexo)
