c09aec777a77395911227de5bf9ebb0150cf323d
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-03-31 15:47:53 -0300

    removendo arquivos com fs.unlink

--- 3-core-modules/8-removendo-arquivos/index.js
+++ 3-core-modules/8-removendo-arquivos/index.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-fs.unlink('arquivo.txt', function (err){
+fs.unlink('arquivo.txt', function(err){
    if(err) {
-        console.log(err)
-        return
-   } 
+      console.log(err)
+      return
+   }
 
    console.log('Arquivo removido!')
 })
