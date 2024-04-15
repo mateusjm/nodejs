5abe3eeedb9df79cb27749758f9d795203761c80
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-04-15 20:38:29 -0300

    enviando dados do usuário através de formulário post

---
+++ 5-express/7-post-de-dados/index.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const path = require('path')
+
+// ler o body
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+)
+
+app.use(express.json())
+
+app.get('/users/add', (req, res) => {
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+app.post('/users/save', (req, res) => {
+    console.log(req.body)
+
+    const name = req.body.name
+    const age = req.body.age
+
+    console.log(`O nome do usuário é ${name} e ele tem ${age} `)
+
+    res.sendFile(`${basePath}/userform.html`)
+})
+
+const basePath = path.join(__dirname, 'templates')
+
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+
+    // leitura da tabela users, resgatar um usuário do banco
+    console.log(`Estamos buscando pelo usuário: ${id}`)
+
+    res.sendFile(`${basePath}/users.html`)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log('App rodando na porta ' + port)
+})
