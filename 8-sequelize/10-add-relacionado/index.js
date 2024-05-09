0437c8daaa963d949d972712e7262befb0a4d9f7
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-05-09 20:47:53 -0300

    adicionando dados relacionados com Sequelize

---
+++ 8-sequelize/10-add-relacionado/index.js
@@ -0,0 +1,146 @@
+// importação pacotes 
+const express = require('express')
+const exphbs = require('express-handlebars')
+const conn = require('./db/conn')
+
+// models
+const User = require('./models/User')
+const Adress = require('./models/Adress')
+
+const app = express()
+
+// body em json
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+// configuração template engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+// arquivos estáticos CSS
+app.use(express.static('public'))
+
+// rota get criar página adduser
+app.get('/users/create', (req,res)=> {
+    res.render('adduser')
+})
+
+// inserir usuário
+app.post('/users/create', async (req,res)=> {
+
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if(newsletter === 'on') {
+        newsletter = true
+    } else {
+        newsletter = false
+    }
+
+    console.log(req.body)
+
+    await User.create({name, occupation, newsletter})
+
+    res.redirect('/')
+})
+
+// criando uma url dinâmica que aceita id pela url
+app.get('/users/:id', async (req, res)=> {
+    const id = req.params.id
+
+    const user = await User.findOne({ raw: true, where : {id: id}})
+
+    res.render('userview', {user})
+})
+
+// excluir usuário
+app.post('/users/delete/:id', async (req,res)=> {
+    const id = req.params.id
+
+    await User.destroy({raw: true, where: {id: id}})
+
+    res.redirect('/')
+})
+
+// renderizar rota useredit
+app.get('/users/edit/:id', async (req, res)=> {
+
+    const id = req.params.id
+
+    const user = await User.findOne({ raw: true, where : {id: id}})
+
+    res.render('useredit', {user})
+})
+
+// rota post da edição dos dados
+app.post('/users/update', async (req, res)=> {
+
+    const id = req.body.id
+    const name = req.body.name
+    const occupation = req.body.occupation
+    let newsletter = req.body.newsletter
+
+    if (newsletter === 'on') {
+        newsletter = true
+    } else {
+        newsletter = false
+    }
+
+    const userData = {
+        id,
+        name,
+        occupation,
+        newsletter
+    }
+
+    await User.update(userData, { where: {id:id}})
+
+    res.redirect('/')
+
+})
+
+// rota home
+app.get('/', async (req, res)=> {
+
+    const users = await User.findAll({raw: true})
+
+    console.log(users)
+
+    res.render('home', {users})
+})
+
+// rota para adicionar endereço
+app.post('/address/create', async (req, res)=> {
+
+    const UserId = req.body.UserId
+    const street = req.body.street
+    const number = req.body.number
+    const city = req.body.city
+
+    const address = {
+        UserId,
+        street,
+        number,
+        city
+    }
+
+    await Adress.create(address)
+
+    res.redirect(`/users/edit/${UserId}`)
+
+})
+
+conn
+    .sync()
+    //.sync({force: true})
+    .then(()=> {
+        app.listen(3000)
+    })
+    .catch((err)=> console.log(err))
+
