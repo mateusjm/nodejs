b4c68beec178434467f2e624abba322c52ea3091
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-05-09 21:52:17 -0300

    Delete conn.js

--- 8-sequelize/11-resgate-relacionados/conn.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize')
-
-const sequelize = new Sequelize('nodesequelize', 'root', '2020', {
-    host: 'localhost',
-    dialect: 'mysql',
-})
-
-module.exports = sequelize
