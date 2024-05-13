118e8fd92343194ff0b9d31d7e36ed6cb0d65ade
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-05-13 18:31:30 -0300

    definindo estrutura MVC para futuros projetos

---
+++ 9-mvc/1-estrutura/index.js
@@ -0,0 +1,29 @@
+// importando os Pacotes para o index
+const express = require('express')
+const exphbs = require('express-handlebars')
+
+// definimos app
+const app = express()
+
+// importando a conexão criada
+const conn = require('./db/conn')
+
+// definimos template engine e propriedades de view engine
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
+
+// middleware para ler o body da requisição
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+// middleware para ler json
+app.use(express.json())
+
+// definindo o uso de arquivos estáticos
+app.use(express.static('public'))
+
+// definindo a porta 
+app.listen(3000)
