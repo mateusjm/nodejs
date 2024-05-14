54273edfba4eced08a84dc7ce4e653a24c7be58d
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-05-14 16:56:09 -0300

    resgatando os dados com findAll e imprimindo no HTML com each

---
+++ 9-mvc/7-resgatando-dados/controllers/TaskController.js
@@ -0,0 +1,35 @@
+// importando o model Task
+const Task = require('../models/Task')
+
+// vamos invocar os models sem instancia-los por métodos estáticos
+module.exports = class TaskController {
+// funções TaskController
+
+    //função criar tarefa
+    static createTask(req, res) {
+        res.render('tasks/create')
+    }
+
+    static async createTaskSave(req, res) {
+
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+            done: false
+        }
+
+        // solicitando o model no Controller
+        await Task.create(task)
+
+        res.redirect('/tasks')
+    }
+
+    // função mostrar tarefas
+    static async showTasks(req, res) {
+
+        const tasks = await Task.findAll({raw: true})
+
+        res.render('tasks/all', {tasks})
+    }
+
+}
---
+++ 9-mvc/7-resgatando-dados/routes/tasksRoutes.js
@@ -0,0 +1,14 @@
+// ipmportando pacotes
+const express = require('express')
+const router = express.Router()
+
+// importando TaskController
+const TaskController = require('../controllers/TaskController')
+
+// criando TaskRoutes a partir das funções TaskController
+router.get('/add', TaskController.createTask)
+router.post('/add', TaskController.createTaskSave)
+router.get('/', TaskController.showTasks)
+
+// exportando router
+module.exports = router
