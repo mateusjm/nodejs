14e59f4f0097cae8730020c16d6be88bef4a71d9
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-05-15 10:54:58 -0300

    resgatando dados para editar com update

---
+++ 9-mvc/9-editar-tarefa/controllers/TaskController.js
@@ -0,0 +1,58 @@
+// importando o model Task
+const { raw } = require('express')
+const Task = require('../models/Task')
+
+// vamos invocar os models sem instancia-los por métodos estáticos
+module.exports = class TaskController {
+// funções TaskController
+
+    //função criar tarefa
+    static createTask(req, res) {
+        res.render('tasks/create')
+    }
+
+    static async createTaskSave(req, res) {
+
+        const task = {
+            title: req.body.title,
+            description: req.body.description,
+            done: false
+        }
+
+        // solicitando o model no Controller
+        await Task.create(task)
+
+        res.redirect('/tasks')
+    }
+
+    // função remover tarefa
+    static async removeTask(req, res) {
+        
+        const id = req.body.id
+
+        await Task.destroy({where: {id: id}})
+
+        res.redirect('/tasks')
+
+    }
+
+    // função editar tarefa
+    static async updateTask(req, res) {
+
+        const id = req.params.id
+
+        const task = await Task.findOne({where: {id: id}, raw:true})
+
+        res.render('tasks/edit', {task})
+
+    }
+
+    // função mostrar tarefas
+    static async showTasks(req, res) {
+
+        const tasks = await Task.findAll({raw: true})
+
+        res.render('tasks/all', {tasks})
+    }
+
+}
---
+++ 9-mvc/9-editar-tarefa/routes/tasksRoutes.js
@@ -0,0 +1,16 @@
+// ipmportando pacotes
+const express = require('express')
+const router = express.Router()
+
+// importando TaskController
+const TaskController = require('../controllers/TaskController')
+
+// criando TaskRoutes a partir das funções TaskController
+router.get('/add', TaskController.createTask)
+router.post('/add', TaskController.createTaskSave)
+router.post('/remove', TaskController.removeTask)
+router.get('/edit/:id', TaskController.updateTask)
+router.get('/', TaskController.showTasks)
+
+// exportando router
+module.exports = router
