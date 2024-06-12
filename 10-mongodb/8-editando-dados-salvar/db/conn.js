a7bf2d2f7a27e8e18e5576c42a46fd45aff45996
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-06-11 21:37:58 -0300

    editando dados método POST com MongoDB

---
+++ 10-mongodb/8-editando-dados-salvar/db/conn.js
@@ -0,0 +1,25 @@
+// importando objeto MongoClient para conectar ao banco
+const { MongoClient } = require('mongodb')
+
+// protocolo do MongoDb mais o IP do nosso servidor
+const uri = "mongodb://127.0.0.1:27017/testemongodb";
+
+// definindo a constante client e passando o uri para a conexão
+const client = new MongoClient(uri)
+
+// função com objetivo de conectar
+async function run() {
+    try {
+        
+        await client.connect()
+        console.log('Conectando ao MongoDB!')
+
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+run()
+
+// exportando client
+module.exports = client
