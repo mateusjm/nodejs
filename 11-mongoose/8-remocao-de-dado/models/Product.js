f2ddd59eb7c3eb75955590b5226f02af931dec4f
Mateus Lucas Joanas Marques <mateuslucas333@gmail.com>
2024-06-13 16:57:55 -0300

    remoção de dado com Mongoose usando deleteOne()

---
+++ 11-mongoose/8-remocao-de-dado/models/Product.js
@@ -0,0 +1,30 @@
+// importando o mongoose
+const mongoose = require('mongoose')
+// importando o Schema do mongoose
+const { Schema } = mongoose
+
+// criando classe Product e inserindo o Schema dentro do método model
+const Product = mongoose.model(
+  'Product',
+  new Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+  }),
+)
+
+// exportando Product
+module.exports = Product
